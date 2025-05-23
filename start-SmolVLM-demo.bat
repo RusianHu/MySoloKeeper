d9121a3c839bbd99ab0eb5c041aadcf3f3804c49
Rusian Huu <hu_bo_cheng@qq.com>
2025-05-24 01:21:17 +0800

    demo增加人脸定位，强制格式化输出

--- start-SmolVLM-demo.bat
+++ start-SmolVLM-demo.bat
@@ -3,6 +3,8 @@ chcp 65001 >nul
 
 echo 启动 SmolVLM-500M-Instruct-f16:
 
-llama-server -m models/SmolVLM-500M-Instruct-f16.gguf --mmproj models/mmproj-SmolVLM-500M-Instruct-f16.gguf -ngl 99
+::llama-server -m models/SmolVLM-500M-Instruct-f16.gguf --mmproj models/mmproj-SmolVLM-500M-Instruct-f16.gguf -ngl 99
+
+llama-server -m models/SmolVLM2-500M-Video-Instruct-f16.gguf --mmproj models/mmproj-SmolVLM2-500M-Video-Instruct-f16.gguf -ngl 99
 
 pause
