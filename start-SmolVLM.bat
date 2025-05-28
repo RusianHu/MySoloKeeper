6acbc648bc30e8b09aeae5c426f68364656ae5ea
Rusian Huu <hu_bo_cheng@qq.com>
2025-05-28 21:13:47 +0800

    更换 smolvlm2-500M 模型

--- start-SmolVLM.bat
+++ start-SmolVLM.bat
@@ -3,8 +3,8 @@ chcp 65001 >nul
 
 echo 启动 SmolVLM2-500M-Instruct-f16:
 
-::llama-server -m models/SmolVLM2-500M-Video-Instruct-f16.gguf --mmproj models/mmproj-SmolVLM2-500M-Video-Instruct-f16.gguf -ngl 99
+llama-server -m models/SmolVLM2-500M-Video-Instruct-f16.gguf --mmproj models/mmproj-SmolVLM2-500M-Video-Instruct-f16.gguf -ngl 99
 
-llama-server -m models/SmolVLM2-2.2B-Instruct-Q4_K_M.gguf --mmproj models/mmproj-SmolVLM2-2.2B-Instruct-f16.gguf -ngl 99
+::llama-server -m models/SmolVLM2-2.2B-Instruct-Q4_K_M.gguf --mmproj models/mmproj-SmolVLM2-2.2B-Instruct-f16.gguf -ngl 99
 
 pause
